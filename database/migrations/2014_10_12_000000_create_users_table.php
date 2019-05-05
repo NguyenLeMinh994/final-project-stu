9e724ce88746f52328e86dda0fa1a8a9da79f964
NguyenLeMinh994 <kurosama99@gmail.com>
2019-05-05 13:56:39 +0700

    Lam lai database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,6 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->engine = 'InnoDB';
-            $table->charset = 'utf8';
             $table->bigIncrements('id');
             $table->string('hoten');
             $table->string('dienthoai')->nullable();
